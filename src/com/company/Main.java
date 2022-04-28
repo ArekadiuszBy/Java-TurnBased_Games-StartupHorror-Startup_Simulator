44d1489be4614f7de979e528370d99da300f0970
Arkadiusz Waw <arekwaw21@gmail.com>
2022-04-28 21:07:43 +0200

    Change main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here Okay
-        System.out.println("Hello world");
+        System.out.println("Hello worldddd");
     }
 }
